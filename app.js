0eb412798b4080bc1cd49199e4a714aaba9f896a
kaustubh0201 <kaustubh1818@gmail.com>
2023-12-13 18:31:50 +0530

    chore: setting up controller, model & routes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 
 const notFoundMiddleware= require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
@@ -14,6 +16,8 @@ app.get('/', (request, response) => {
     response.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
 
+app.use('/api/v1/products', productsRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
@@ -22,6 +26,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_DB_URI);
+        console.log("Connected to the database.");
         app.listen(port, console.log(`Server is listening on port ${port}.`));
     } catch (error) {
         console.log(error);
